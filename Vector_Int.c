22c2b19e643080f549dc9173e190b8e4e52991e4
Shivansh Gupta <guptashivansh5119@gmail.com>
2023-11-23 22:16:55 +0530

    Added Better Comments

--- Vector_Int.c
+++ Vector_Int.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Defining the different colors of text for the terminal just so it looks nice
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
@@ -15,7 +16,8 @@
 #define ANSI_COLOR_BRIGHT_GREEN "\x1b[92m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-void copyIntArrays(int* a,int* b , int length)
+// A simple fuction which copies the contents of a to b
+void copyIntArrays (int* a, int* b , int length)
 {
     for(int i = 0; i < length ; i++)
     {
@@ -23,6 +25,7 @@ void copyIntArrays(int* a,int* b , int length)
     }
 }
 
+// Reserving the space in the array and initializing its default value as 0
 void Vector_Int_Rsrv (Vector_Int* v,int space)
 {
     v->array = malloc ( sizeof(int) * space);
@@ -34,6 +37,7 @@ void Vector_Int_Rsrv (Vector_Int* v,int space)
         printf(ANSI_COLOR_RED "\nCouldn't allocate memory" ANSI_COLOR_RESET);
         return;
     }
+
     // Setting Default Value to 0
     // Initializing the length var
     for (int i = 0; i < space ; i++)
@@ -44,17 +48,18 @@ void Vector_Int_Rsrv (Vector_Int* v,int space)
     printf(ANSI_COLOR_BRIGHT_GREEN "Memory allocated Successfully !\n" ANSI_COLOR_RESET);
 }
 
+// Adding an integer in an existing array 
 void Vector_Int_Push (Vector_Int* v, int x)
 {
-    // Checking if the array is FRESH for efficient code
+    // Checking if the array is FRESH 
     if ( ( v->array[0] == 0 ) && (( v->length * sizeof(int) ) == sizeof(int) ) )
     {
         v->array[0] = x;
         return;
     }
 
-    // Getting size and length of the present array
-    int size = (v->length) * sizeof(int); // No. of values 
+    // Size of array in bytes 
+    int size = (v->length) * sizeof(int); 
     
     // Creating a new buffer to store the present values of array
     int* temp = malloc (size);  
@@ -62,11 +67,11 @@ void Vector_Int_Push (Vector_Int* v, int x)
     copyIntArrays (v->array , temp, v->length); 
 
     // Freeing the present array so as to store
-    // New values in a bigger array
+    // new values in a bigger array
     free(v->array);
 
     // Allocating the required memory size to add 
-    // Given int value to the array
+    // given int value to the array
     v->array = malloc (size + sizeof(int));
     copyIntArrays (temp , v->array, v->length); // copying old data
 
@@ -78,11 +83,14 @@ void Vector_Int_Push (Vector_Int* v, int x)
     free(temp);
 }
 
+// Destroying the Vector once its job is done
 void Vector_Int_Destroy (Vector_Int* v)
 {
     free(v->array);
+    v->length = 0;
 }
 
+// A print function for printing a Int Vector
 void Vector_Int_Print (Vector_Int* v)
 {
     printf(ANSI_COLOR_BLUE "===============================\n" ANSI_COLOR_RESET);
--- Vector_Int.h
+++ Vector_Int.h
@@ -10,22 +10,27 @@ typedef struct {
     int length;
 } Vector_Int;
 
+// A function for copying int arrays for convinience
 void copyIntArrays(
     int* a,int* b , int length
 );
 
+// Reserve space in vector and initialize its default values as 0
 void Vector_Int_Rsrv(
     Vector_Int* v,int space
 );
 
+// Adding values to the Vector
 void Vector_Int_Push(
     Vector_Int* v, int x
 );
 
+// Finally, Destroying the vector
 void Vector_Int_Destroy (
     Vector_Int* v
 );
 
+// A function for printing the int vector
 void Vector_Int_Print(
     Vector_Int* v
 );
